6dc4bdebf2815640587dbb7975eef37e8a164f9c
Muthukumaran.b <muthukumaran.b@vuedata.in>
2024-10-21 09:20:42 +0530

    Email service done

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ const connectDB = () => {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME, // Replace with your database name
+        database: process.env.DB_NAME,
     });
 
     connection.connect(err => {
